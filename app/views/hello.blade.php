2361f5d85f57c0add31a0e03d05385ef85cb1fd3
Francis Soares de Oliveira <franciscpd@iMac-de-Francis.local>
2013-07-08 08:25:47 -0400

    Removido arquivos de configuração ambiente local

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -8,5 +8,21 @@
 
 {{-- Page content --}}
 @section('content')
-
+    <div class="hero-unit">
+        <h1>Hello world!</h1>
+        <p>
+            Esse é um projeto open-source para inicialização de um projeto, criado
+            com o objetivo de auxiliar no desenvolvimento de novos projetos, sem
+            precisar ficar reconfigurando o básico de todo o sistema.
+        </p>
+        <p>
+            Caso tenha interesse em nos auxiliar no desenvolvimento acesse o nosso
+            projeto no GitHub.
+        </p>
+        <p>
+            <a class="btn btn-primary btn-large" href="https://github.com/franciscpd/skeleton-laravel" target="_blank">
+                Acessar Projeto &raquo;
+            </a>
+        </p>
+    </div>
 @stop
